c8a781b5b279ca9b8560838014610cccba00d303
achihoub2023 <anis.chihoub@gmail.com>
2021-06-05 01:08:22 -0400

    added more data visualization stuff

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template,request,redirect,flash
 import sqlite3,requests
 from model import *
 from PIL import Image 
-import numpy as np,os,glob
+import numpy as np,os,glob,pandas as pd
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import ImmutableMultiDict
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -38,7 +39,7 @@ def data_viz():
             conn.close()    
             output_string = "The tuple with the highest value of selection,"+selection+": "+str(ret)
             return render_template('data_viz.html',ret = output_string,ret2='')
-        if 'min' in data:
+        elif 'min' in data:
             selection = str(data.get('Attribute')[0])
             res = 'SELECT * FROM houses WHERE '+selection+'= (Select MIN('+selection+') FROM houses)'
             results = cur.execute(res)
@@ -48,6 +49,21 @@ def data_viz():
             
             output_string = "The tuple with the lowest value of selection,"+selection+": "+str(ret)
             return render_template('data_viz.html',ret = '',ret2 = output_string)
+        elif 'submit_graphs' in data:
+            data = request.form
+            data = data.to_dict(flat=False)
+            selection_one = str(data.get('vals')[0])
+            selection_two = str(data.get('vals')[1])
+            df = pd.read_csv('static/data/out.csv')
+            x = df[selection_one]
+            y = df[selection_two]
+            plt.scatter(x,y)
+            plt.xlabel(selection_one)
+            plt.ylabel(selection_two)
+            title = "Scatter plot between "+selection_one+" and "+selection_two
+            plt.title(title)
+            plt.savefig('static/created/created.png')
+            return render_template('data_viz.html',fig='succ') 
     else:
         return render_template('data_viz.html') 
 
@@ -56,7 +72,7 @@ def predict():
     file = request.files['img']
     if file.filename == '':
         flash('No image here')
-        return redirect('signals.html')
+        return redirect('classification.html')
     else:
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -67,7 +83,7 @@ def predict():
     input_img = np.asarray(im)
     input_img = input_img[None,:]
     preds = model(input_img)
-    return render_template('signals.html',prediction_text = preds)
+    return render_template('classification.html',prediction_text = preds)
     
 @app.route('/')
 def index():
